dbf49041d4d060b5a36ad4b07732a2add1167d3e
Michael Nutt <michael@nuttnet.net>
2015-03-03 11:36:38 -0800

    add emscripten compiler

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+var EmscriptenCompiler = require('./lib/emscripten-compiler');
+
 module.exports = {
-  name: 'ember-cli-emscripten'
+  name: 'Ember CLI Emscripten Addon',
+
+  included: function(app) {
+    app.registry.add('js', {
+      name: 'ember-cli-emscripten',
+      ext: 'js',
+
+      toTree: function(tree, inputPath, outputPath) {
+        var options = {
+          srcDir: inputPath,
+          destDir: outputPath
+        };
+        return EmscriptenCompiler(tree, options);
+      }
+    });
+  }
 };
